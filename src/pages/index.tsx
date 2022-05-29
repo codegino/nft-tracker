8d15c830fd249b55f9bad86557be9a00d58659b2
codegino <carloginocatapang@gmail.com>
2022-05-29 18:48:13 +0800

    feat(watchlist): added card view and table view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import {useEffect, useState} from 'react';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 import Layout from '../components/Layout';
 
 type CollectionStats = {
   name: string;
-  floorPrice: string;
-  oneDaySales: string;
+  floorPrice: number;
+  oneDaySales: number;
   oneDayAveragePrice: number;
   oneDayVolume: number;
   oneDayChange: number;
@@ -17,6 +18,187 @@ type CollectionStats = {
 };
 
 const toTwoDecimalPlaces = (num: number) => Math.round(num * 100) / 100;
+
+const SorterColumn = ({prop, children}) => {
+  const {query} = useRouter();
+
+  const [sortProp, sortOrder] = (query?.sortBy as string).split(':');
+
+  return (
+    <th className="p-2 text-left">
+      <Link
+        href={{
+          pathname: '/',
+          query: {
+            ...query,
+            sortBy: `${prop}:${
+              sortProp === prop ? (sortOrder === 'asc' ? 'desc' : 'asc') : 'asc'
+            }`,
+          },
+        }}
+      >
+        <a className={sortProp === prop ? 'text-blue-700' : ''}>
+          {children}
+          {sortProp === prop ? (sortOrder === 'desc' ? '▼' : '▲') : ''}
+        </a>
+      </Link>
+    </th>
+  );
+};
+
+const TableView = ({collectionsStats}) => {
+  return (
+    <table>
+      <thead>
+        <tr className="border-y-2 border-blue-900">
+          <SorterColumn prop="name">Name</SorterColumn>
+          <SorterColumn prop="floorPrice">Floor Price</SorterColumn>
+          <SorterColumn prop="oneDaySales">One Day Sales</SorterColumn>
+          <th className="p-2 text-left">One Day Average Price</th>
+          <th className="p-2 text-left">One Day Volume</th>
+          <th className="p-2 text-left">24h%</th>
+          <th className="p-2 text-left">7d%</th>
+          <SorterColumn prop="totalVolume">Total Volume</SorterColumn>
+        </tr>
+      </thead>
+      <tbody>
+        {collectionsStats.map(collection => {
+          return (
+            <tr key={collection.slug} className="border-b-2 border-blue-900">
+              <td className="p-3 text-left">{collection.name}</td>
+              <td className="p-3 text-left">{collection.floorPrice}⬙</td>
+              <td className="p-3 text-left">
+                {toTwoDecimalPlaces(collection.oneDaySales)}
+              </td>
+              <td className="p-3 text-left">
+                {toTwoDecimalPlaces(collection.oneDayAveragePrice)}⬙
+              </td>
+              <td className="p-3 text-left">
+                {toTwoDecimalPlaces(collection.oneDayVolume)}⬙
+              </td>
+              <td
+                className={
+                  collection.oneDayChange > 0
+                    ? 'text-green-500'
+                    : 'text-red-500'
+                }
+              >
+                {toTwoDecimalPlaces(collection.oneDayChange)}%
+              </td>
+              <td
+                className={
+                  collection.sevenDayChange > 0
+                    ? 'text-green-500'
+                    : 'text-red-500'
+                }
+              >
+                {toTwoDecimalPlaces(collection.sevenDayChange)}%
+              </td>
+              <td className="p-3 text-left">
+                {toTwoDecimalPlaces(collection.totalVolume)}⬙
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+const CardView = ({collectionsStats}) => {
+  return (
+    <div>
+      {collectionsStats.map(collection => {
+        return (
+          <div key={collection.slug} className="mb-2">
+            <Link href={`https://opensea.io/collection/${collection.slug}`}>
+              <a target="_blank">
+                <h3 className="text-xl">
+                  Name: <b>{collection.name}</b>
+                </h3>
+              </a>
+            </Link>
+            <h4>
+              Floor Price: <b>{collection.floorPrice}</b>
+            </h4>
+            <h4>
+              One Day Sales: <b>{collection.oneDaySales}</b>
+            </h4>
+            <h4>
+              One Day Average Price:{' '}
+              <b>{toTwoDecimalPlaces(collection.oneDayAveragePrice)}</b>
+            </h4>
+            <h4>
+              One Day Volume:{' '}
+              <b>{toTwoDecimalPlaces(collection.oneDayVolume)}</b>
+            </h4>
+            <h4>
+              24h %:{' '}
+              <b
+                className={
+                  collection.oneDayChange > 0
+                    ? 'text-green-500'
+                    : 'text-red-500'
+                }
+              >
+                {toTwoDecimalPlaces(collection.oneDayChange)}%
+              </b>
+            </h4>
+            <h4>
+              7d%:{' '}
+              <b
+                className={
+                  collection.sevenDayChange > 0
+                    ? 'text-green-500'
+                    : 'text-red-500'
+                }
+              >
+                {toTwoDecimalPlaces(collection.sevenDayChange)}%
+              </b>
+            </h4>
+            <h4>
+              Total Volume: <b>{toTwoDecimalPlaces(collection.totalVolume)}</b>
+            </h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const ViewChanger = () => {
+  const {query} = useRouter();
+
+  return (
+    <div className="my-4">
+      {query?.view === 'table' ? (
+        <Link
+          href={{
+            pathname: '/',
+            query: {
+              ...query,
+              view: 'card',
+            },
+          }}
+        >
+          <a>Switch to Card view</a>
+        </Link>
+      ) : (
+        <Link
+          href={{
+            pathname: '/',
+            query: {
+              ...query,
+              view: 'table',
+            },
+          }}
+        >
+          <a>Switch to Table view</a>
+        </Link>
+      )}
+    </div>
+  );
+};
 
 const IndexPage = () => {
   const [collections] = useState([
@@ -39,66 +221,44 @@ const IndexPage = () => {
       });
   }, [collections]);
 
+  const {query} = useRouter();
+
+  const sortedCollectionStats = query?.sortBy
+    ? collectionsStats.sort((a, b) => {
+        const [sortProp, order] = (query.sortBy as string).split(':');
+
+        if (sortProp === 'name') {
+          if (order === 'asc') {
+            return b[sortProp].localeCompare(a[sortProp]);
+          } else {
+            return a[sortProp].localeCompare(b[sortProp]);
+          }
+        }
+
+        if (order === 'asc') {
+          return b[sortProp] - a[sortProp];
+        }
+        return a[sortProp] - b[sortProp];
+      })
+    : collectionsStats;
+
   return (
     <Layout title="OpenSea floor price">
       <h1 className="text-3xl font-bold underline mb-4">My Watchlist</h1>
-      <div>
-        {collectionsStats.map(collection => {
-          return (
-            <div key={collection.slug} className="mb-2">
-              <Link href={`https://opensea.io/collection/${collection.slug}`}>
-                <a target="_blank">
-                  <h3 className="text-xl">
-                    Name: <b>{collection.name}</b>
-                  </h3>
-                </a>
-              </Link>
-              <h4>
-                Floor Price: <b>{collection.floorPrice}</b>
-              </h4>
-              <h4>
-                One Day Sales: <b>{collection.oneDaySales}</b>
-              </h4>
-              <h4>
-                One Day Average Price:{' '}
-                <b>{toTwoDecimalPlaces(collection.oneDayAveragePrice)}</b>
-              </h4>
-              <h4>
-                One Day Volume:{' '}
-                <b>{toTwoDecimalPlaces(collection.oneDayVolume)}</b>
-              </h4>
-              <h4>
-                24h %:{' '}
-                <b
-                  className={
-                    collection.oneDayChange > 0
-                      ? 'text-green-500'
-                      : 'text-red-500'
-                  }
-                >
-                  {toTwoDecimalPlaces(collection.oneDayChange)}%
-                </b>
-              </h4>
-              <h4>
-                7d%:{' '}
-                <b
-                  className={
-                    collection.sevenDayChange > 0
-                      ? 'text-green-500'
-                      : 'text-red-500'
-                  }
-                >
-                  {toTwoDecimalPlaces(collection.sevenDayChange)}%
-                </b>
-              </h4>
-              <h4>
-                Total Volume:{' '}
-                <b>{toTwoDecimalPlaces(collection.totalVolume)}</b>
-              </h4>
-            </div>
-          );
-        })}
-      </div>
+
+      {collectionsStats.length > 0 && <ViewChanger />}
+
+      {collectionsStats.length === 0 ? (
+        <h2 className="text-2xl">Fetching Collection Stats...</h2>
+      ) : (
+        <>
+          {query?.view === 'table' ? (
+            <TableView collectionsStats={sortedCollectionStats} />
+          ) : (
+            <CardView collectionsStats={sortedCollectionStats} />
+          )}
+        </>
+      )}
     </Layout>
   );
 };
