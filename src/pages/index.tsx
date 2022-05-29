03dfcdabeecc46bd611e6ec8deca2092c692e951
codegino <carloginocatapang@gmail.com>
2022-05-29 09:44:44 +0800

    Initialized project from boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1 className="text-3xl font-bold underline">
+      Hello Next.js with TypeScript and Tailwind 👋
+    </h1>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
